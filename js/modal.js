992101be7b04dd2cfc24e83e11fb97b4cf4927d0
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-06 01:18:15 +0300

    udate

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,31 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const modals = document.querySelectorAll('[data-modal]');
+const openModalBtns = document.querySelectorAll('[data-modal-open]');
+const closeModalBtns = document.querySelectorAll('[data-modal-close]');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+for (let i = 0; i < modals.length; i++) {
+  openModalBtns[i].addEventListener('click', openModal.bind(null, i));
+  closeModalBtns[i].addEventListener('click', closeModal.bind(null, i));
+}
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+function openModal(index) {
+  modals[index].classList.remove('is-hidden');
+}
+
+function closeModal(index) {
+  modals[index].classList.add('is-hidden');
+}
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
